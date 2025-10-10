eff47d98f0883f311f043a81c4edede6ec3d5aa0
AhmedEssam <ae.devverse@gmail.com>
2025-10-10 21:32:54 +0300

    Feat: Create products section

---
+++ src/components/common/ProductsSection.tsx
@@ -0,0 +1,22 @@
+import ProductCard from "@/components/common/ProductCard";
+import { Product } from "@/data/products";
+
+interface ProductsSectionProps {
+  products: Product[];
+}
+
+function ProductsSection({ products }: ProductsSectionProps) {
+  return (
+    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-base">
+      {products
+        .filter((product) => !product.isPlus)
+        .map((product) => (
+          <li key={product.id} className="">
+            <ProductCard product={product} />
+          </li>
+        ))}
+    </ul>
+  );
+}
+
+export default ProductsSection;
