697285310e72aefb7942166c5c8be852d929ff72
AhmedEssam <ae.devverse@gmail.com>
2025-10-10 21:37:00 +0300

    Feat: Create ProductsSection component

--- src/components/common/ProductsSection.tsx
+++ src/components/common/ProductsSection.tsx
@@ -7,7 +7,7 @@ interface ProductsSectionProps {
 
 function ProductsSection({ products }: ProductsSectionProps) {
   return (
-    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-base">
+    <ul className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-2 lg:gap-base">
       {products
         .filter((product) => !product.isPlus)
         .map((product) => (
