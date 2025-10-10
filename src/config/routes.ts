59040d86561cdf44a86a8a392c4c64a4b985d3df
AhmedEssam <ae.devverse@gmail.com>
2025-10-10 14:51:43 +0300

    Feat: Create routes data file as a single source for all routes

---
+++ src/config/routes.ts
@@ -0,0 +1,17 @@
+interface Routes {
+  home: string;
+  menu: string;
+  shop: string;
+  locations: string;
+  contact: string;
+}
+
+const ROUTES: Routes = {
+  home: "/",
+  menu: "/menu",
+  shop: "/shop",
+  locations: "/locations",
+  contact: "/contact",
+};
+
+export default ROUTES;
