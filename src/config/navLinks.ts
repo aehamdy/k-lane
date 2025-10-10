8ebb99ff7cd3b9a016fd6fc08f82f1f7cdbb8ffd
AhmedEssam <ae.devverse@gmail.com>
2025-10-10 14:53:02 +0300

    Feat: Create navLinks data file as a single source of navbar links

---
+++ src/config/navLinks.ts
@@ -0,0 +1,31 @@
+import ROUTES from "@/config/routes";
+
+const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    href: ROUTES.home,
+  },
+  {
+    id: 2,
+    name: "Menu",
+    href: ROUTES.menu,
+  },
+  {
+    id: 3,
+    name: "Shop",
+    href: ROUTES.shop,
+  },
+  {
+    id: 4,
+    name: "Locations",
+    href: ROUTES.locations,
+  },
+  {
+    id: 5,
+    name: "Contact",
+    href: ROUTES.contact,
+  },
+];
+
+export default navLinks;
