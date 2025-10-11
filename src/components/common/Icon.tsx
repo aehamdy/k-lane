c3c25b951208c89e4f5427ae4c3c97cfdd3dc3fa
AhmedEssam <ae.devverse@gmail.com>
2025-10-11 04:40:56 +0300

    Feat: Add new icons for 'locations' page

--- src/components/common/Icon.tsx
+++ src/components/common/Icon.tsx
@@ -1,15 +1,28 @@
 import { CgMenuMotion } from "react-icons/cg";
 import { GoHeartFill } from "react-icons/go";
 import { IoClose } from "react-icons/io5";
+import { LuClock3, LuMail, LuMapPin, LuPhone } from "react-icons/lu";
 import { PiStarFourFill } from "react-icons/pi";
 
-type IconName = "menu" | "close" | "star" | "heart";
+type IconName =
+  | "menu"
+  | "close"
+  | "star"
+  | "heart"
+  | "mapPin"
+  | "clock"
+  | "phone"
+  | "mail";
 
 const iconMap = {
   menu: CgMenuMotion,
   close: IoClose,
   star: PiStarFourFill,
   heart: GoHeartFill,
+  mapPin: LuMapPin,
+  clock: LuClock3,
+  phone: LuPhone,
+  mail: LuMail,
 };
 
 interface IconProps {
