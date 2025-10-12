f4a249ca1d97e30f72596103e81205c9317ff2aa
AhmedEssam <ae.devverse@gmail.com>
2025-10-13 02:18:47 +0300

    feat: add HeaderWrapper component

---
+++ src/components/common/HeaderWrapper.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/common/Header";
+
+function HeaderWrapper() {
+  return (
+    <div className="absolute start-1/2 top-base -translate-x-1/2 w-[92%] lg:w-[96%] z-50">
+      <Header />
+    </div>
+  );
+}
+
+export default HeaderWrapper;
