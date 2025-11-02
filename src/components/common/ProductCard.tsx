c519eb8ba466a54c821750e691372528fbcaf42e
Ahmed Essam <ae.devverse@gmail.com>
2025-11-02 13:20:28 +0200

    Feat: Wrap product card with link that redirects to the product's page upon click on it

--- src/components/common/ProductCard.tsx
+++ src/components/common/ProductCard.tsx
@@ -1,36 +1,37 @@
 import Heading from "@/components/common/Heading";
+import ROUTES from "@/config/routes";
 import { Product } from "@/data/products";
 import Image from "next/image";
+import Link from "next/link";
 
 function ProductCard({ product }: { product: Product }) {
   return (
-    <article
-      key={product.id}
-      className="flex flex-col bg-light rounded-base overflow-hidden"
-    >
-      <div>
-        <Image
-          src={product.image}
-          alt={product.title}
-          className="w-full h-full object-cover"
-        />
-      </div>
-
-      <div className="flex justify-between items-center p-base text-dark">
-        <div className="flex flex-col gap-tiny text-dark">
-          <Heading level={3} className="font-archivo font-black text-core">
-            {product.title}
-          </Heading>
-
-          <p className="font-inter font-medium text-small text-deep">
-            {product.subtitle}
-          </p>
+    <article key={product.id} className="bg-light rounded-base overflow-hidden">
+      <Link href={`${ROUTES.shop}/${product.slug}`} className="flex flex-col">
+        <div>
+          <Image
+            src={product.image}
+            alt={product.title}
+            className="w-full h-full object-cover"
+          />
         </div>
 
-        <div className="p-3 bg-soft rounded-xl">
-          <p className="font-semibold text-small">${product.price}</p>
+        <div className="flex justify-between items-center p-base text-dark">
+          <div className="flex flex-col gap-tiny text-dark">
+            <Heading level={3} className="font-archivo font-black text-core">
+              {product.title}
+            </Heading>
+
+            <p className="font-inter font-medium text-small text-deep">
+              {product.subtitle}
+            </p>
+          </div>
+
+          <div className="p-3 bg-soft rounded-xl">
+            <p className="font-semibold text-small">${product.price}</p>
+          </div>
         </div>
-      </div>
+      </Link>
     </article>
   );
 }
